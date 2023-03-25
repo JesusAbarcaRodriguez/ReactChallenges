67b52e443272d299b50e5d449f25e7deca6f6a10
ALCHACAS <alvaro.chaves.cascante@est.una.ac.cr>
2023-03-25 11:39:53 -0600

    [UPDATE] DELETE FILES

--- reactChallenges/src/App.jsx
+++ reactChallenges/src/App.jsx
@@ -1,6 +1,4 @@
 import React, { useState } from "react";
-import UserList from "./component/userList";
-import UserForm from "./component/userForm";
 import First from "./component/firstChallenge"
 function App() {
   const [user, setUser] = useState([]);
@@ -11,8 +9,6 @@ function App() {
 
   return (
     <div class=" box-border bg-gray-700 to-#F6F1F1 rounded shadow h-0 flex flex-col justify-center">
-      <UserForm onSubmit={addUser} />
-      <UserList users={user} />
       <First/>
     </div>
   );
