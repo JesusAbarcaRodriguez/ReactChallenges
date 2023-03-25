02d795bb19a9d54e4ada72e21b8a3d086d8f4379
JesusAbarca <jesus.abarca.rodriguez@est.una.ac.cr>
2023-03-25 11:13:21 -0600

    [UPDATE component]
    Se añadio nuevamente la carpeta component

---
+++ reactChallenges/src/component/firstChallenge.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import './index.css'
+
+function firstChallenge() {
+    return (
+        <h1>NUESTROS APELLIDOS</h1>
+    );
+}
+
+export default firstChallenge;
